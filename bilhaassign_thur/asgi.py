ab47a8e18d5ea2ac41385815aa01aab89f430ba1
bilhaaketch <bilhaaketch13@gmail.com>
2023-07-28 09:23:22 +0300

    Initial commit

---
+++ bilhaassign_thur/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bilhaassign_thur project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bilhaassign_thur.settings')
+
+application = get_asgi_application()
---
+++ student_db/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from student_db.models import Student
+
+
+# Register your models here.
+admin.site.register(Student)
